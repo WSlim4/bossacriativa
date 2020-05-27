5921779e4fcdd274ffb555b7b28091393f9f8ea4
Mauricio Alves <mauricio_lalves@hotmail.com>
2020-05-27 00:38:30 -0300

    +banner na home

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../../global.css';
 import './Home.css';
+import Banner from '../../assets/banner-1.jpg';
 
 function Home() {
 
   return (
     <div className="home-container">
+      <div className="banner-div">
+        <img src={Banner} alt="Logo" className="banner"/>
+      </div>
     </div>
   );
 }
