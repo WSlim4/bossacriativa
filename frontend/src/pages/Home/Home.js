0b01cbfc38a25bd0414165abd2ce339b7b8be0f1
Mauricio Alves <mauricio_lalves@hotmail.com>
2020-05-28 21:01:21 -0300

    +listagem de videoaulas

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -56,6 +56,45 @@ function Home() {
           </Row>
         </div>
       </div>
+
+      <div className="video-list-div">
+        <div className="title-div">
+          <h2 className="title-home">VIDEOAULAS EM DESTAQUE</h2>
+        </div>
+        <div className="video-list">
+          <Row>
+            <Col sm={3}>
+              <div className="video">
+                <h4 className="videoaula">VÍDEO AULA</h4>
+                <h5 className="instrumento">Violão</h5>
+                <h6 className="aula">Composição</h6>
+              </div>
+            </Col>
+            <Col sm={3}>
+              <div className="video">
+                <h4 className="videoaula">VÍDEO AULA</h4>
+                <h5 className="instrumento">Violão</h5>
+                <h6 className="aula">Composição</h6>
+              </div>
+            </Col>
+            <Col sm={3}>
+              <div className="video">
+                <h4 className="videoaula">VÍDEO AULA</h4>
+                <h5 className="instrumento">Violão</h5>
+                <h6 className="aula">Composição</h6>
+              </div>
+            </Col>
+            <Col sm={3}>
+              <div className="video">
+                <h4 className="videoaula">VÍDEO AULA</h4>
+                <h5 className="instrumento">Violão</h5>
+                <h6 className="aula">Composição</h6>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+
     </div>
   );
 }
