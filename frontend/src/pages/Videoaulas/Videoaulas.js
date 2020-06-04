58c251a447fe9e7ab144ae4a8afc2486dc1e7152
Mauricio Alves <mauricio_lalves@hotmail.com>
2020-06-03 21:56:49 -0300

    + página de videoaulas

--- frontend/src/pages/Videoaulas/Videoaulas.js
+++ frontend/src/pages/Videoaulas/Videoaulas.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import '../../global.css';
 import './Videoaulas.css';
-import { Container } from 'react-grid-system';
+import { Container, Row, Col } from 'react-grid-system';
 
 function Videoaulas() {
     return (
         <div className="videoaulas-container">
-            <Container>
-                <div className="videoaulas-div">
-                <h1 className="title-videoaulas">VÍDEOAULAS</h1>
-                <h3 className="subtitle-videoaulas">Em breve</h3>
+            <div className="video-list-div">
+                <div className="title-div">
+                <h2 className="title-home">VIDEOAULAS EM DESTAQUE</h2>
                 </div>
-            </Container>
+                <div className="video-list">
+                <Row>
+                    <Col sm={3}>
+                    <div className="video">
+                        <h4 className="videoaula">VÍDEO AULA</h4>
+                        <h5 className="instrumento">Violão</h5>
+                        <h6 className="aula">Composição</h6>
+                    </div>
+                    </Col>
+                    <Col sm={3}>
+                    <div className="video">
+                        <h4 className="videoaula">VÍDEO AULA</h4>
+                        <h5 className="instrumento">Violão</h5>
+                        <h6 className="aula">Composição</h6>
+                    </div>
+                    </Col>
+                    <Col sm={3}>
+                    <div className="video">
+                        <h4 className="videoaula">VÍDEO AULA</h4>
+                        <h5 className="instrumento">Violão</h5>
+                        <h6 className="aula">Composição</h6>
+                    </div>
+                    </Col>
+                    <Col sm={3}>
+                    <div className="video">
+                        <h4 className="videoaula">VÍDEO AULA</h4>
+                        <h5 className="instrumento">Violão</h5>
+                        <h6 className="aula">Composição</h6>
+                    </div>
+                    </Col>
+                </Row>
+                </div>
+            </div>
         </div>
     
     )
