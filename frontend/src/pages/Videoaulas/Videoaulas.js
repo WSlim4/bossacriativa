65a4a2b47f5293be290ab57a99635695421a5c07
Wesley Lima <wesleyjoga500@gmail.com>
2020-06-04 19:34:26 -0300

    + paginação

--- frontend/src/pages/Videoaulas/Videoaulas.js
+++ frontend/src/pages/Videoaulas/Videoaulas.js
@@ -5,13 +5,61 @@ import { Row, Col } from 'react-grid-system';
 import api from '../../services/api'
 import ReactPlayer from 'react-player'
 
-function VideoAulas(){
-      const [lessons, setLessons] = useState([])
+class VideoAulas extends React.Component{
+     constructor(props){
+         super(props)
+         this.state={
+             data: [],
+             perPage: 8
+         }
+         this.__onClick = this.__onClick.bind(this)
+     }
+     async loadData(page){
+        const response = await api.get(`/lessons?page=${page}`)
+         this.setState({ total: response.data.total })
+         this.setState({ data: response.data.data })
+         this.setState({ pageNumbers: Math.ceil(this.state.total / this.state.perPage)})
+     }
+     range(start, stop, step) {
+        if (typeof stop == 'undefined') {
 
-      useEffect(()=>{
-        api.get('/lessons').then(res=>setLessons(res.data.data))
-    }, [])
-
+            stop = start;
+            start = 0;
+        }
+    
+        if (typeof step == 'undefined') {
+            step = 1;
+        }
+    
+        if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+            return [];
+        }
+    
+        var result = [];
+        for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+            result.push(i);
+        }
+    
+        return result;
+    };
+    componentDidMount(){
+        const { page } = this.props.match.params
+        this.loadData(page)
+     }
+    
+    componentDidUpdate(prevProps){
+        const { page } = this.props.match.params
+        if( page !== prevProps.match.params.page ){
+            this.loadData(page)
+        }
+     }
+     __onClick(val){
+        this.loadData(val)
+     }
+     
+      render(){
+          const pgNmb = this.range(1,this.state.pageNumbers+1)
+          const lessons = this.state.data
       return (
         <div className="videoaulas-container">
             <div className="video-contents">
@@ -31,10 +79,14 @@ function VideoAulas(){
                         </li>
                         )}
                     </ul>
+                    {pgNmb.map(val=>
+                        <button value={val} className="btns" onClick={() => this.__onClick(val)} >{val}</button>
+                    )}
                 </div>
             </div>
     
-    )
+        )
+    }
 }
-  export default VideoAulas;
+export default VideoAulas;
   
