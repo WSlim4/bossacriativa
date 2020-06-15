88d17bfbd4d62232a636f89d31d71121c5afffff
Danilo Collares <dandee@localhost.localdomain>
2020-06-15 18:17:07 -0300

    *menu para mobile - login

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import {NavLink, Link} from 'react-router-dom';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import '../../global.css';
 import './Navbar.css';
 import logonav from '../../assets/logotipo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class NavBar extends Component {
     render() {
@@ -13,9 +14,9 @@ class NavBar extends Component {
                 <Link to="/" className="logo">
                     <img src={logonav} alt="Logo" className="brand-logo"/>
                 </Link>
-                <Nav>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><NavLink className="link" to="/login">LOGIN</NavLink></li>
+                <Nav className='menuItem' id="largeMenuItem">
+                <ul id="nav-mobile" className="d-none d-lg-block">
+                    {/* <li><NavLink className="link" to="/login">LOGIN</NavLink></li> */}
                     <li><NavLink className="link" to="/about">O PROJETO</NavLink></li>
                     <li><NavLink className="link" to="/palestras">PALESTRAS</NavLink></li>
                     <li><NavLink className="link" to="/shows">SHOWS</NavLink></li>
@@ -24,13 +25,27 @@ class NavBar extends Component {
                     <li><NavLink className="link" to="/imprensa">IMPRENSA</NavLink></li> 
                     <li><NavLink className="link" to="/editais">EDITAIS</NavLink></li>
                 </ul>
-                <div className="menu-toggle">
+                {/* <div className="menu-toggle">
                     <div className="one"></div>
                     <div className="two"></div>
                     <div className="three"></div>
-                </div> 
+                </div> */}
+                </Nav>
+                <Nav className='d-lg-none menuItem'>
+                    <NavDropdown title="Menu">
+                        {/* <NavDropdown.Item><NavLink className="link" to="/login">LOGIN</NavLink></NavDropdown.Item> */}
+                        <NavDropdown.Item><NavLink className="link" to="/about">O PROJETO</NavLink></NavDropdown.Item>
+                        <NavDropdown.Item><NavLink className="link" to="/palestras">PALESTRAS</NavLink></NavDropdown.Item>
+                        <NavDropdown.Item><NavLink className="link" to="/shows">SHOWS</NavLink></NavDropdown.Item>
+                        <NavDropdown.Item><NavLink className="link" to="/videoaulas">VIDEOAULAS</NavLink></NavDropdown.Item>
+                        <NavDropdown.Item><NavLink className="link" to="/midia">MIDIA</NavLink></NavDropdown.Item>
+                        <NavDropdown.Item><NavLink className="link" to="/imprensa">IMPRENSA</NavLink></NavDropdown.Item>
+                        <NavDropdown.Item><NavLink className="link" to="/editais">EDITAIS</NavLink></NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
             </Navbar>
+            
+                    
             </>
 
             
