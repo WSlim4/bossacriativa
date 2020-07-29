55a3a64e65efe79be65d24c57aacd13546283470
Wesley Lima <wesleyjoga500@gmail.com>
2020-07-29 16:28:49 -0300

    deploy

--- backend/app/Controllers/Http/NewsController.js
+++ backend/app/Controllers/Http/NewsController.js
@@ -38,9 +38,9 @@ class NewsController {
     async index ({ request }){
         let { page } = request.all()
         page = page ? page : 1
-        const news = Database.table('news')
+        const news = Database.table('news').orderBy('id', 'desc')
         
-        return await news.paginate(page ? page : 1, 6)
+        return await news.paginate(page ? page : 1, 30)
     }
 
     async show ({ params }){
