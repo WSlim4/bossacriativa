ce842049319a278a63ea3bb5b7fb4d5d17f2c4fb
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2020-08-27 21:59:40 -0300

    Alteraçao da quantidade de noticias a serem exibidas

--- backend/app/Controllers/Http/NewsController.js
+++ backend/app/Controllers/Http/NewsController.js
@@ -51,7 +51,7 @@ class NewsController {
     async lastNews({ request }){
         let { page } = request.all()
         page = page ? page : 1
-        const news = Database.table('news').limit(4).orderBy('id', 'desc')
+        const news = Database.table('news').limit(3).orderBy('id', 'desc')
         
         return await news
     }
