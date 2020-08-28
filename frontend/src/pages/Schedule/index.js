423290c34ec209b4a75bae6dfe1d4ffa667b0082
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2020-08-28 20:11:50 -0300

    Correçao dos elementos da agenda

--- frontend/src/pages/Schedule/index.js
+++ frontend/src/pages/Schedule/index.js
@@ -33,7 +33,7 @@ export default function Schedule() {
                     <img src={require('../../assets/seta.svg')} height="20px" />
                   </button>
                 </div>
-                <div style={{ marginLeft: 10, marginRight: 10, backgroundColor: '#efefef', padding: '3px 15px' }}>
+                <div style={{ marginLeft: 10, marginRight: 10, backgroundColor: '#efefef', padding: '3px 15px', height: 34, display: 'flex', alignItems: 'center' }}>
                   {months.length > 0 ? months[month - 1].name : ''}
                 </div>
                 <div>
@@ -64,7 +64,7 @@ export default function Schedule() {
             ))
             }
         </Row>
-        <Row className="home-content">
+        <Row>
           <Col lg={12}>
             <div className="slt-buttons">
               <span>
@@ -72,7 +72,7 @@ export default function Schedule() {
                   <img src={require('../../assets/seta.svg')} height="20px" />
                 </button>
               </span>
-              <span style={{ marginLeft: 10, marginRight: 10, backgroundColor: '#efefef', padding: '3px 15px' }}>
+              <span style={{ marginLeft: 10, marginRight: 10, backgroundColor: '#efefef', padding: '3px 15px', height: 34, display: 'flex', alignItems: 'center' }}>
                 {months.length > 0 ? months[month - 1].name : ''}
               </span>
               <span>
