5295b7d006cd76151da3779c8b3c3990a45c3f28
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2020-09-12 08:01:49 -0300

    Mudança de api da lista de noticias

--- frontend/src/pages/Noticia/Noticia.js
+++ frontend/src/pages/Noticia/Noticia.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import './Noticia.css';
 import { Link } from 'react-router-dom';
-import api from '../../services/api'
+import strapi from '../../services/strapi';
 
 class Noticia extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            news: []
+            news: {},
+            baseUrl: 'https://admin.bossacriativa.art.br'
         }
     }
 
     async componentDidMount(){
-        const response = await api.get(`/news/${this.props.match.params.id}`)
-        this.setState({ news: response.data})
+      strapi.get(`/noticias/${this.props.match.params.id}`)
+        .then(({ data }) => this.setState({ news: data }));
+      console.log(this.props.match.params.id);
     }
 
     render(){
       return (
         <div className="container">
             <h6 className="breadcrumb"><span className="colorspan"><Link className="link" to="/noticias">NOTÍCIAS</Link><span className="fontspan"> - {this.state.news.title}</span></span></h6>
-            <img className="img-not" src={this.state.news.img_url}/>
+            <img 
+              className="img-not" 
+              src={
+                this.state.news.Cover 
+                  ? `${this.state.baseUrl}${this.state.news.Cover.formats.large.url}`
+                  : ''
+              }
+            />
             <div className="text-about-div">
                 <h1 className="title-about">
                     {this.state.news.title}
                 </h1>
                 <p className="text-about">
-                    <td dangerouslySetInnerHTML={{__html: this.state.news.description}} />
+                    <td dangerouslySetInnerHTML={{__html: this.state.news.text}} />
                 </p>
             </div>
             <Link className="link" to="/noticias"><a className="volte btn">Voltar para Notícias</a></Link>
--- frontend/src/pages/Noticias/Noticias.js
+++ frontend/src/pages/Noticias/Noticias.js
@@ -1,60 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import './Noticias.css';
-import api from '../../services/api'
-import history from '../../services/history'
+import { Link } from 'react-router-dom';
+import strapi from '../../services/strapi';
 
-function Noticias() {
-    const [newses, setNews] = useState([])
-    const [filteredNews, setFiltered] = useState([])
-    const [filter, setFilter] = useState([])
+export default function Noticias() {
+  const [newses, setNews] = useState([])
+  const [filter, setFilter] = useState([])
+  const [baseUrl, ] = useState('https://admin.bossacriativa.art.br');
     
-    useEffect(()=>{
-        async function loadData(){
-            const response = await api.get('/news')
-            setNews(response.data.data)
-        }
-        loadData()
-    }, [])
+  useEffect(()=>{
+    strapi.get(`/noticias?date_lte=${Date.now()}`).then(({ data }) => setNews(data));
+  }, [])
 
-    async function search(e){
-        e.preventDefault()
-
-        const news = await api.get('/searchNews',{
-            params:{
-                value: filter
-            }
-        })
-        setFiltered(news.data)
-    }
-
-    return (
-        <div className="home-content cont">
-                <div className="head title">
-                    <h2>NOTÍCIAS</h2>
-                    <div className="filters">
-                        <form onSubmit={()=>search()}>
-                            <div id="buscar">
-                                <label htmlFor="buscar">BUSCAR:</label>
-                                <input type="text" name="buscar" placeholder="Digite aqui para buscar" onChange={(e)=>setFilter(e.target.value)}/>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div className="not-content"> 
-                {newses.map(news=>
-                    <div style={{ backgroundColor: "#E7C032"}}>
-                        <div className="div-img news-img" style={{backgroundImage: `url(${news.img_url})`}}/>
-                        <div>
-                          <h6 className="not-title" style={{backgroundColor: 'white'}}>{news.title}</h6>
-                          <p style={{fontSize: ' 1.2em'}}>{news.introduction}</p>
-                          <a className="leia btn nws-btn" onClick={()=> { history.push(`/noticia/${news.id}`)}}>Leia mais</a>
-                        </div>
-                    </div>
-                    )}
-                </div>
-            </div>
-    )
+  async function search(e){
+    e.preventDefault();
+    const query = `/noticias?title_contains=${filter}&date_lte=${Date.now()}`;
+    const { data } = await strapi.get(query);
+    setNews(data);
   }
-  
-  export default Noticias;
-  
+
+  return (
+    <div className="home-content cont">
+      <div className="head title">
+        <h2>NOTÍCIAS</h2>
+        <div className="filters">
+          <form onSubmit={()=>search()}>
+            <div id="buscar">
+              <label htmlFor="buscar">BUSCAR:</label>
+              <input 
+                type="text" 
+                name="buscar" 
+                placeholder="Digite aqui para buscar" 
+                onChange={(e)=>setFilter(e.target.value)}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="not-content"> 
+        {
+          newses.map(news => (
+            <div style={{ backgroundColor: "#E7C032"}}>
+              <div 
+                className="div-img news-img" 
+                style={{backgroundImage: `url(${news.Cover ? `${baseUrl}${news.Cover.formats.thumbnail.url}` : ''})`}}
+              />
+              <div>
+                <h6 className="not-title" style={{backgroundColor: 'white'}}>
+                  {news.title}
+                </h6>
+                <p style={{fontSize: '1.2em'}}>{news.intro}</p>
+                <Link
+                  to={`/noticia/${news.id}`}
+                  className="leia btn nws-btn" 
+                >
+                  Leia mais
+                </Link>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+};  
